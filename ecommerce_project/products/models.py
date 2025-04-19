7b26a46e3ae191b5236c55012948649592a6e8c6
max theocharis <maxteo1985@gmail.com>
2025-04-20 00:54:56 +0300

    adding the product models

--- ecommerce_project/products/models.py
+++ ecommerce_project/products/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    name     = models.CharField(max_length=200)
+    slug     = models.SlugField(max_length=200, unique=True)
+    image    = models.ImageField(upload_to='products/')
+    description = models.TextField(blank=True)
+    price    = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+    created   = models.DateTimeField(auto_now_add=True)
+    updated   = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('-created',)
+
+    def __str__(self):
+        return self.name
