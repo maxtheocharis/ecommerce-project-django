b29840e61c51e0794ac8fb0480adace9a91e602d
max theocharis <maxteo1985@gmail.com>
2025-04-20 01:03:23 +0300

    create models for car

--- ecommerce_project/cart/models.py
+++ ecommerce_project/cart/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from products.models import Product
 
-# Create your models here.
+class CartItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def sub_total(self):
+        return self.product.price * self.quantity
