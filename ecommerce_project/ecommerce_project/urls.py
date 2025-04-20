940b3325f948c28157292130585924735667fb47
max theocharis <maxteo1985@gmail.com>
2025-04-20 18:49:55 +0300

    alter the ecommerce urls.py file

--- ecommerce_project/ecommerce_project/urls.py
+++ ecommerce_project/ecommerce_project/urls.py
@@ -1,10 +1,20 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # Users & auth
     path('users/', include('users.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),  # login/logout/password
+
+    # E‑commerce
     path('products/', include('products.urls')),
     path('cart/', include('cart.urls')),
     path('orders/', include('orders.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
