3dff022ab6d7724534578f0c27f84d219f2e3b7f
max theocharis <maxteo1985@gmail.com>
2025-04-20 18:56:52 +0300

    write the view.py from orders

--- ecommerce_project/orders/views.py
+++ ecommerce_project/orders/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import Order
+from cart.views import cart_detail  # you might refactor this
+
+@login_required
+def order_create(request):
+    cart = request.session.get('cart', {})
+    if not cart:
+        return redirect('product_list')
+    order = Order.objects.create(
+        user=request.user,
+        shipping_address=request.POST.get('shipping_address', '')
+    )
+    for slug, qty in cart.items():
+        product = get_object_or_404(Product, slug=slug)
+        item = CartItem.objects.create(product=product, quantity=qty)
+        order.items.add(item)
+    request.session['cart'] = {}
+    return redirect('order_detail', pk=order.pk)
+
+@login_required
+def order_detail(request, pk):
+    order = get_object_or_404(Order, pk=pk)
+    return render(request, 'orders/order_detail.html', {'order': order})
+
 
 # Create your views here.
