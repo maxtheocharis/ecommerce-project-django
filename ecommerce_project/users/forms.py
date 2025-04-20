e85d04504b6094361247338520b75ee16083b0e8
max theocharis <maxteo1985@gmail.com>
2025-04-20 18:51:15 +0300

    alter the users forms.py

--- ecommerce_project/users/forms.py
+++ ecommerce_project/users/forms.py
@@ -3,10 +3,8 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class SignUpForm(UserCreationForm):
-    email = forms.EmailField(
-        required=True,
-        help_text="Required. Enter a valid email address."
-    )
+    email = forms.EmailField(required=True,
+                             help_text="Required. Enter a valid email address.")
 
     class Meta:
         model  = User
