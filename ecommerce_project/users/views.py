bb14078942fd0bdd968803886fdb9f18fa5b62e4
max theocharis <maxteo1985@gmail.com>
2025-04-20 17:29:01 +0300

    create users/views

--- ecommerce_project/users/views.py
+++ ecommerce_project/users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import SignUpForm
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            messages.success(
+                request,
+                f'Account created for {user.username}! You can now log in.'
+            )
+            return redirect('login')
+    else:
+        form = SignUpForm()
+
+    return render(request, 'users/register.html', {'form': form})
