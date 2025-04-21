8dca3ef6ffb192c94ac649a36c1a5b6308162307
max theocharis <maxteo1985@gmail.com>
2025-04-21 15:47:37 +0300

    add the product/views.py file

--- ecommerce_project/products/views.py
+++ ecommerce_project/products/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+# products/views.py
 
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+from .models import Product
+
+def product_list(request):
+    """
+    Show all available products.
+    """
+    products = Product.objects.filter(available=True)
+    return render(request, 'products/product_list.html', {
+        'products': products
+    })
+
+def product_detail(request, slug):
+    """
+    Show a single product’s detail page, looked up by slug.
+    """
+    product = get_object_or_404(Product, slug=slug, available=True)
+    return render(request, 'products/product_detail.html', {
+        'product': product
+    })
